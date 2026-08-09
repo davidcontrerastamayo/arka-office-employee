902e36ff41452c69d3baec5980264f71f3f7505b
Luis David Contreras Tamayo <luis@MacBook-Pro-de-Luis.local>
2026-08-08 22:28:37 -0500

    Renamed App.js to App.jsx to enable JSX syntax support.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
+import './styles.css'; // Asegúrate de que los estilos se importen correctamente
 
 const rootElement = document.getElementById('root');
-
-if (rootElement) {
-    const root = ReactDOM.createRoot(rootElement);
-    root.render(<React.StrictMode><App /></React.StrictMode>);
-} else {
-    console.error('Elemento raíz no encontrado.');
+if (!rootElement) {
+    throw new Error('No root element found');
 }
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(<React.StrictMode><App /></React.StrictMode>);
